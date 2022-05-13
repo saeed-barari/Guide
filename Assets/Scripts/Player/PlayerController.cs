c74d5919b034cf0520cd9dd9cf1eb5d5aa00303f
IpGa13 <83000938+IpGa13@users.noreply.github.com>
2022-05-13 17:17:13 +0200

    FPS Controller
    Simple FPS Controller to get started. Not Final.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    //Invisible Variables
+    Rigidbody player_rb;
+    bool movementLock;
+    bool cameraLock;
+    //public bool lockMouse;
+
+    [Header("Parameters")]
+    [SerializeField] private float playerSpeed = 10f;
+    [SerializeField] private float cameraSensitivityMultiplier = 1f;
+
+    [Header("Assignables")]
+    [SerializeField] private Camera fpsCam;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        player_rb = GetComponent<Rigidbody>();
+        //lockMouse = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!movementLock) {
+            Move();
+        }
+
+        if (!cameraLock) {
+            LookAround();
+        }
+
+        //if (lockMouse) {
+        //    Cursor.lockState = CursorLockMode.Locked;
+        //} else {
+        //    Cursor.lockState = CursorLockMode.None;
+        //}
+
+    }
+
+    void Move() {
+        float xMove = Input.GetAxis("Horizontal");
+        float zMove = Input.GetAxis("Vertical");
+
+        transform.Translate(playerSpeed * Vector3.forward * Time.deltaTime * zMove);
+        transform.Translate(playerSpeed * Vector3.right * Time.deltaTime * xMove);
+    }
+
+    void LookAround() {
+        float vertMouse = -Input.GetAxis("Mouse Y")*cameraSensitivityMultiplier;
+        float horizMouse = Input.GetAxis("Mouse X")*cameraSensitivityMultiplier;
+        // TODO: Clamp the Vertical Rotation
+        gameObject.transform.Rotate(0, horizMouse, 0);
+        fpsCam.transform.Rotate(vertMouse, 0, 0);
+    }
+}
